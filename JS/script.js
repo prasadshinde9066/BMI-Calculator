16a5bc004a49f7919f9d1f94fdb9d296658e6639
prasadshinde9066 <prasadshinde14256@gmail.com>
2022-02-21 21:53:23 +0530

    Inital Commit

---
+++ JS/script.js
@@ -0,0 +1,22 @@
+function calculateBMI(){
+    var weight = parseFloat(document.getElementById("txt-weight").value);
+    var heightCm = parseFloat(document.getElementById("txt-height").value);
+    var heightM = heightCm/100;
+
+    var bmi = (weight/(heightM*heightM)).toFixed(2);
+
+    document.getElementById("txt-bmi").innerHTML = "BMI: "+bmi;
+
+    if(bmi<18.5){
+        document.getElementById("txt-reamark").innerHTML = "underweight";
+    }
+    else if(bmi >= 18.5 && bmi <= 24.9){
+        document.getElementById("txt-remark").innerHTML = "Normal";
+    }
+    else if(bmi >=25 && bmi <=29.9){
+        document.getElementById(txt-remark).innerHTML = "Overweight";
+    }
+    else if(bmi >= 30){
+        document.getElementById("txt-remark").innerHTML = "Obese";
+    }
+}
