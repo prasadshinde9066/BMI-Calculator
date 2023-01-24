7dbd3fdd87fc2efcf84ff1447d022d49040c5ad3
Prasad Shinde <99635675+prasadshinde9066@users.noreply.github.com>
2023-01-24 19:09:25 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+function calculateBMI() {
+  var weight = parseFloat(document.getElementById("txt-weight").value);
+  var heightCm = parseFloat(document.getElementById("txt-height").value);
+  var heightM = heightCm / 100;
+
+  var bmi = (weight / (heightM * heightM)).toFixed(2);
+
+  document.getElementById("txt-bmi").innerHTML = "BMI: " + bmi;
+
+  if (bmi < 18.5) {
+    document.getElementById("txt-remark").innerHTML = "Underweight";
+    document.getElementById("progress-remark").className =
+      "progress-bar progress-bar-striped progress-bar-animated";
+  } else if (bmi >= 18.5 && bmi <= 24.9) {
+    document.getElementById("txt-remark").innerHTML = "Normal";
+    document.getElementById("progress-remark").className =
+      "progress-bar progress-bar-striped progress-bar-animated bg-success";
+  } else if (bmi >= 25 && bmi <= 29.9) {
+    document.getElementById("txt-remark").innerHTML = "Overweight";
+    document.getElementById("progress-remark").className =
+      "progress-bar progress-bar-striped progress-bar-animated bg-warning";
+  } else if (bmi >= 30) {
+    document.getElementById("txt-remark").innerHTML = "Obese";
+    document.getElementById("progress-remark").className =
+      "progress-bar progress-bar-striped progress-bar-animated bg-danger";
+  }
+}
